0d0826e137dc52dcee9a4270277549afab25c249
MRTANKO <mr.kozeko@mail.ru>
2018-10-16 22:56:29 +1000

    Rename

--- Practice_1/zadanie 1.1.py
+++
@@ -1,9 +0,0 @@
-time = input()
-cloc=int(time)//60
-min=int(time)%60
-if cloc< 24:
-    print(cloc)
-    print(min)
-else:
-    print(cloc%24)
-    print(min)
--- Practice_1/zadanie 1.2.py
+++
@@ -1,9 +0,0 @@
-v=int(input())
-t=int(input())
-s=v*t
-if 0<=s<109:
-    print(s)
-elif s>=109 or s<-109:
-    print(s%109)
-elif -109<=s<0:
-    print(109+s)
